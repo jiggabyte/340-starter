5803b2d834f6631e1a5e31df6e74ba2ad1416ba0
Jigga Byte <jigga.e10@gmail.com>
2025-03-25 16:53:20 +0100

    ok

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ app.use(async (err, req, res, next) => {
 const port = process.env.PORT;
 const host = process.env.HOST;
 
+
 /* ***********************
  * Log statement to confirm server operation
  *************************/
